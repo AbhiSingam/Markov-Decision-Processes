5be975f1d91aec48a324d473b0c7de125e9556c4
WafflesThe1st <abhisingam2@gmail.com>
2021-04-08 16:35:50 +0530

    Created output files for part 3

--- part_3.py
+++ part_3.py
@@ -1,7 +1,9 @@
 import cvxpy as cp
 import numpy as np
 import pickle
-from part_2 import get_prob, validActions, STEPCOST, posDic
+import json
+import os
+from part_2 import get_prob, validActions, STEPCOST, posDic, positionMap, mstateMap
 # x = cp.Variable(shape=(2, 1), name="x")
 # A = np.array([[4, 3], [-3, 4]])
 LOCATIONS = 5
@@ -13,6 +15,23 @@ state_to_idx = np.zeros((LOCATIONS, MAX_MATS, MAX_ARROWS, MON_STATES, MAX_HEALTH
 idx_to_state = []
 idx_to_state_action = []
 
+def get_policy(x):
+    policy = []
+    for state, _ in np.ndenumerate(state_to_idx):
+        best_action = 'NONE'
+        best_x = np.NINF
+        # print(len(idx_to_state_action))
+        for idx, st_act in enumerate(idx_to_state_action):
+            if st_act[0] == state and x.value[idx][0] > best_x:
+                best_action = st_act[1]
+                best_x = x.value[idx][0]
+        # return best_action
+        modified_state = list(state).copy()
+        modified_state[0] = positionMap[modified_state[0]].upper()
+        modified_state[4] = modified_state[4] * 4
+        modified_state[3] = mstateMap[modified_state[3]]
+        policy.append([tuple(modified_state), best_action])
+    return policy
 
 def calcReward(state, prevState):
     if prevState[posDic['mstate']] == 1 and state[posDic['mstate']] == 0 and state[0] in [0, 3] and state[posDic['mhealth']] > 0:
@@ -33,6 +52,7 @@ def init_states():
 
 def get_A():
     A = np.zeros((len(idx_to_state),len(idx_to_state_action)))
+    A_list = []
     for idx, st_act in enumerate(idx_to_state_action):
         if st_act[1] == 'NONE':
             A[int(state_to_idx[st_act[0]])][idx] = 1
@@ -41,7 +61,9 @@ def get_A():
             for i, prob in enumerate(probs):
                 A[int(state_to_idx[st_act[0]])][idx] += prob
                 A[int(state_to_idx[tuple(states[i])])][idx] -= prob
-    return A
+    for i in A:
+        A_list.append(list(i))
+    return A, A_list
 
 def get_R():
     R = np.zeros(len(idx_to_state_action))
@@ -67,22 +89,50 @@ def solve(A, alpha, r):
     problem = cp.Problem(objective, constraints)
 
     solution = problem.solve()
-    print(solution)
+    # print(solution)
 
-    print(x.value)
+    # print(x.value)
 
     with open('lp.pkl', 'wb') as fd:
         pickle.dump(x.value, fd)
+    
+    x_list = []
+    for i in x.value:
+        x_list.append(list(i))
+    
+    return x, x_list, solution
 
 if __name__ == '__main__':
     start_state = (0,0,0,0,100)
     general_case = True
     init_states()
     print(len(idx_to_state_action))
-    A = get_A()
+    A, A_list = get_A()
     R = get_R()
     alpha = get_alpha(general_case, start_state)
-    solve(A, alpha, R)
+
+    alpha_list = []
+    for i in alpha:
+        alpha_list.append(list(i))
+
+    x, x_list, objective = solve(A, alpha, R)
+    # print(x.value)
+    policy = get_policy(x)
+    # print(type(list(policy)[0][0]))
+    # print(type(list(x.value)[0]))
+    to_jsonify = {
+        "a" : A_list,
+        "r" : list(R),
+        "alpha" : alpha_list,
+        "x" : x_list,
+        "policy" : list(policy),
+        "objective" : float(objective)
+    }
+
+    os.makedirs("outputs", exist_ok = True)
+
+    with open("outputs/part_3_output.json", "w") as f:
+        json.dump(to_jsonify, f)
     # x = cp.Variable(shape=(len(idx_to_state_action), 1), name="x")
     # cp.matmul(A,x)
     # print(idx_to_state_action)
