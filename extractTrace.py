804d06b86fc909b54f6a1b5719fff915e974cdd0
saravanan senthil <senthil_saravanan@rocketmail.com>
2021-04-08 19:44:38 +0530

    made plot things

--- extractTrace.py
+++ extractTrace.py
@@ -2,6 +2,7 @@ import pickle
 import numpy as np
 import os
 from part_2 import positionMap
+from matplotlib import pyplot as plt
 
 with open('a.pkl', 'rb') as fd:
     hist, actions = pickle.load(fd)
@@ -18,10 +19,16 @@ mState = ['D', 'R']
 #make outputdir
 os.makedirs('outputs', exist_ok=True)
 path = './outputs/part_2_trace.txt'
+deltas = []
+
 
 with open(path, 'w') as fd:
     for j in range(len(hist)  - 1):
         i = j+1
+        t1 = hist[i]
+        t2 = hist[j]
+        diff = np.max(np.abs(t1 - t2))
+        deltas.append(diff)
         print('iteration=' + str(j), file=fd)
         for state, _ in np.ndenumerate(hist[i]):
             # print(state)
@@ -29,3 +36,6 @@ with open(path, 'w') as fd:
             # print(actions[i][state].astype(str))
             print(' :' + actions[i][state].astype(str) + '=[' + str(round(hist[i][state],2)) + ']', file=fd)
 
+plt.plot(deltas)
+plt.show()
+print(deltas)
