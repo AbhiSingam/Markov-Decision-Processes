2824f944af76e677b1a5bdd042689104a29a496f
saravanan senthil <senthil_saravanan@rocketmail.com>
2021-04-08 23:38:23 +0530

    thing

---
+++ submission_part2/extractTrace.py
@@ -0,0 +1,44 @@
+import pickle
+import numpy as np
+import os
+from part_2 import positionMap
+from matplotlib import pyplot as plt
+
+with open('a.pkl', 'rb') as fd:
+    hist, actions = pickle.load(fd)
+
+pos = [-1] * 5
+for idx, i in enumerate(positionMap):
+    # print(idx, i)
+    pos[idx] = i.upper()
+
+mState = ['D', 'R']
+
+# print(pos)
+
+#make outputdir
+os.makedirs('outputs', exist_ok=True)
+path = './outputs/part_2_trace.txt'
+deltas = []
+
+
+with open(path, 'w') as fd:
+    for j in range(len(hist)  - 1):
+        i = j+1
+        t1 = hist[i]
+        t2 = hist[j]
+        diff = np.max(np.abs(t1 - t2))
+        deltas.append(diff)
+        print('iteration=' + str(j), file=fd)
+        for state, _ in np.ndenumerate(hist[i]):
+            # print(state)
+            print('(' + pos[state[0]] + ',' + str(state[1])+ ',' + str(state[2]) + ',' + mState[state[3]] + ',' + str(state[4] * 25) + ')', end='', file=fd)
+            # print(actions[i][state].astype(str))
+            print(' :' + actions[i][state].astype(str) + '=[' + str(round(hist[i][state],2)) + ']', file=fd)
+
+plt.plot(deltas, label='delta')
+plt.legend()
+plt.xlabel('iteration')
+plt.ylabel('diff')
+plt.show()
+print(deltas)
---
+++ submission_part2/simulation.py
@@ -0,0 +1,54 @@
+import pickle
+import random
+import numpy
+from part_2 import get_prob, positionMap
+
+with open('a.pkl', 'rb')as fd:
+    hist, histActions = pickle.load(fd)
+
+
+pos = [-1] * 5
+for idx, i in enumerate(positionMap):
+    # print(idx, i)
+    pos[idx] = i.upper()
+
+mState = ['D', 'R']
+
+utils = hist[-1]
+thisAction = histActions[-1]
+
+positionMap = ['c', 'n', 's', 'e', 'w']
+posDic = {
+    'pos': 0,
+    'mat': 1,
+    'arrow': 2,
+    'mstate': 3,
+    'mhealth': 4,
+}
+
+def update_state(state, action2):
+    # print(type(action2[0]))
+    # print(action2[0][0])
+    prob, states = get_prob(state, action2)
+    rand = random.random()
+    for i in range(len(prob) - 1):
+        prob[i + 1] += prob[i]
+        
+    for i, pro in enumerate(prob):
+        if rand <= pro:
+            return states[i] 
+
+# 1.(W, 0, 0, D, 100) 2.(C, 2, 0, R, 100)
+startState = (4, 0, 1, 0, 4)
+# startState = (0, 2, 0, 1, 4)
+done = False
+while not done:
+    printState = list(startState)
+    printState[0] = pos[printState[0]]
+    printState[3] = mState[printState[3]]
+    printState[4] *= 25
+    print('state: ', tuple(printState), ';action: ', thisAction[tuple(startState)])
+    startState = update_state(list(startState), thisAction[tuple(startState)])
+
+    if startState[posDic['mhealth']] == 0:
+        done = True
